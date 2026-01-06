1e140bafecda88c5a76b96fe8cd16a0f47aba4d2
kivejun <1523304166@qq.com>
2026-01-06 16:26:07 +0800

    修改readme,并配置启动文件

--- scripts/xtdrone_run_stereo.sh
+++ scripts/xtdrone_run_stereo.sh
@@ -1,1 +1,2 @@
-rosrun ORB_SLAM3 Stereo_Inertial ~/catkin_ws/src/ORB_SLAM3/Vocabulary/ORBvoc.txt ~/catkin_ws/src/ORB_SLAM3/Examples/Stereo-Inertial/px4_sitl.yaml true /camera/left/image_raw:=/iris_0/stereo_camera/left/image_raw /camera/right/image_raw:=/iris_0/stereo_camera/right/image_raw /orbslam2/vision_pose/pose:=/iris_0/mavros/vision_pose/pose
+rosrun ORB_SLAM3 Stereo /home/kivejun/catkin_ws/src/ORB_SLAM3/Vocabulary/ORBvoc.txt /home/kivejun/catkin_ws/src/ORB_SLAM3/Examples/Stereo-Inertial/px4_sitl.yaml true /camera/left/image_raw:=/solo_0/stereo_camera/left/image_raw /camera/right/image_raw:=/solo_0/stereo_camera/right/image_raw
+
